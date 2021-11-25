fb39e64c7098583d5b3e426b16cec706324a0a17
mrdoodles <19146299+mrdoodles@users.noreply.github.com>
2021-11-25 16:40:10 +0000

    feat(actions): Automated semantic release versioning.
    * feat(actions): Automated semantic release versioning.
    
    * fix(actions): implement semantic versioning with conventional commits.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Github Actions/i);
+  const linkElement = screen.getByText(/learn Github Actions Test App/i);
   expect(linkElement).toBeInTheDocument();
 });
