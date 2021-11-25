2265ace046bf1f3a6e02ff5a13aed32354b497b1
Tim Preece <19146299+mrdoodles@users.noreply.github.com>
2021-11-25 15:33:25 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn Github Actions
+          Learn Github Actions Test App
         </a>
       </header>
     </div>
