f7730c8a595238a29e05ac20caa6982c6f34b30a
Tim Preece <19146299+mrdoodles@users.noreply.github.com>
2021-11-26 17:40:00 +0000

    fix(component): fix issue with application title

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn Github Actions Test
+          Learn Github Actions Test App
         </a>
       </header>
     </div>
