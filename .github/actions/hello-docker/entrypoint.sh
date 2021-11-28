581c18493037db3333afae79c7ca286b363a57d4
Tim Preece <19146299+mrdoodles@users.noreply.github.com>
2021-11-28 11:28:41 +0000

    fix(ci): change hello to docker action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+echo "::add-mask::$1"
+
+echo "Hello $1"
+time=$(date)
+
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable group"
+echo "Some stuff"
+echo "Some stuff"
+echo "Some stuff"
+echo "::endGroup::"
+
+echo "::set-env name=HELLO::hello"
