45c560a980161ce6454050555970bbb12cd8b14d
Tim Preece <19146299+mrdoodles@users.noreply.github.com>
2021-11-28 18:21:07 +0000

    fix(ci): test static

--- .github/actions/slack-message/src/slack.php
+++ .github/actions/slack-message/src/slack.php
@@ -4,8 +4,8 @@ define('REQUESTS_SILENCE_PSR0_DEPRECATIONS', true);
 require_once 'vendor/autoload.php';
 Requests::register_autoloader();
 
-var_dump($argv);
-var_dump($_ENV);
+// var_dump($argv);
+// var_dump($_ENV);
 
 // echo "::debug::Sending a request to Slack\n";
 
@@ -21,7 +21,7 @@ $response = Requests::post(
                     "type" => "section",
                     "text" => array(
                         "type" => "mrkdwn",
-                        "text" => $_ENV['INPUT_SLACK_MESSAGE'],
+                        "text" => "You have a new request: *<fakeLink.toEmployeeProfile.com|Fred Basset - New collar request>*",
                     ),
                 ),
                 array(
@@ -29,23 +29,47 @@ $response = Requests::post(
                     "fields" => array(
                         array(
                             "type" => "mrkdwn",
-                            "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+                            "text" => "*Type:*\nComputer (laptop)",
                         ),
                         array(
                             "type" => "mrkdwn",
-                            "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
-                        ),
-                        array(
-                            "type" => "mrkdwn",
-                            "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
-                        ),
-                        array(
-                            "type" => "mrkdwn",
-                            "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
-                        ),
-                    ),
-                ),
-            ),
+                            "text" => "*When:*\nSubmitted Aut 10",
+                        )
+                    )
+                )
+            )
+    // json_encode(array(
+    //     'blocks' =>
+    //         array(
+    //             array(
+    //                 "type" => "section",
+    //                 "text" => array(
+    //                     "type" => "mrkdwn",
+    //                     "text" => $_ENV['INPUT_SLACK_MESSAGE'],
+    //                 ),
+    //             ),
+    //             array(
+    //                 "type" => "section",
+    //                 "fields" => array(
+    //                     array(
+    //                         "type" => "mrkdwn",
+    //                         "text" => "*Repository:*\n{$_ENV['GITHUB_REPOSITORY']}",
+    //                     ),
+    //                     array(
+    //                         "type" => "mrkdwn",
+    //                         "text" => "*Event:*\n{$_ENV['GITHUB_EVENT_NAME']}",
+    //                     ),
+    //                     array(
+    //                         "type" => "mrkdwn",
+    //                         "text" => "*Ref:*\n{$_ENV['GITHUB_REF']}",
+    //                     ),
+    //                     array(
+    //                         "type" => "mrkdwn",
+    //                         "text" => "*SHA:*\n{$_ENV['GITHUB_SHA']}",
+    //                     ),
+    //                 ),
+    //             ),
+    //         ),
 
     ))
 );
